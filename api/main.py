d529bca1e267c3c971f5857ee0eed933e56f8156
maxim.shnyagin <maxim.shnyagin@latoken.com>
2023-06-10 15:22:05 +0300

    downgrade mongodb

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from internal.controllers import user, chat
@@ -9,3 +10,7 @@ app = FastAPI()
 
 app.include_router(router=user.router)
 app.include_router(router=chat.router)
+
+app.get("/env/{key}")
+async def get_env(key: str):
+    return os.getenv(key)
