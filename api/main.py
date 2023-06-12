258ccae2d7cef00c24568abbdf0f72a21eb0ee36
maxim.shnyagin <maxim.shnyagin@latoken.com>
2023-06-12 16:10:36 +0300

    allow cors

--- api/main.py
+++ api/main.py
@@ -2,11 +2,24 @@ import os
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
 from internal.controllers import user, chat
+
+from fastapi.middleware.cors import CORSMiddleware
+
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(router=user.router)
 app.include_router(router=chat.router)
